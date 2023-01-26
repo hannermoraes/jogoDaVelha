8b358e6779ef78882f3946205682fafbeea3f0e9
hannermoraes <hannermoraes@gmail.com>
2023-01-26 17:16:22 -0300

    Finalização parcial do projeto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const board = document.querySelector("[data-board]");
 const cellElements = document.querySelectorAll("[data-cell]");
 const winningMessage = document.querySelector("[data-winning-message]");
@@ -33,7 +32,7 @@ const startGame = () => {
 };
 const endGame = (isDraw) => {
   if (isDraw) {
-    winningMessageTextElement.innerText = 'Deu Velha'
+    winningMessageTextElement.innerText = 'Deu Velha!'
   } else {
     winningMessageTextElement.innerText = isCircleTurn ? 'O  Venceu!' : 'X  Venceu!';
   };
@@ -91,98 +90,4 @@ const handleClick = (e) => {
   }
 };
 startGame();
-=======
-const board = document.querySelector("[data-board]");
-const cellElements = document.querySelectorAll("[data-cell]");
-const winningMessage = document.querySelector("[data-winning-message]");
-const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
-const restartButton = document.querySelector("[data-restart-button]");
-
-
-let isCircleTurn;
-
-const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
-
-const startGame = () => {
-  isCircleTurn = false;
-
-  for (const cell of cellElements) {
-    cell.classList.remove("circle");
-    cell.classList.remove("x");
-    cell.removeEventListener("click", handleClick);
-    cell.addEventListener("click", handleClick, { once: true });
-  };
-  setBoardHoverClass();
-  winningMessage.classList.remove("show-winning-message");
-};
-const endGame = (isDraw) => {
-  if (isDraw) {
-    winningMessageTextElement.innerText = 'Empate'
-  } else {
-    winningMessageTextElement.innerText = isCircleTurn ? 'O  Venceu!' : 'X  Venceu!';
-  };
-  winningMessage.classList.add("show-winning-message");
-};
-
-const checkForWin = (currentPlayer) => {
-  return winningCombinations.some((combination) => {
-    return combination.every((index) => {
-      return cellElements[index].classList.contains(currentPlayer);
-    });
-  });
-};
-const checkForDraw = () => {
-  return [...cellElements].every((cell) => {
-    return cell.classList.contains("x") || cell.classList.contains("circle");
-  });
-};
-const placeMark = (cell, classToAdd) => {
-  cell.classList.add(classToAdd);
-};
-const setBoardHoverClass = () => {
-  board.classList.remove('circle');
-  board.classList.remove('x');
-
-  if (isCircleTurn) {
-    board.classList.add('circle');
-  } else {
-    board.classList.add('x');
-  };
-};
-const swapTurns = () => {
-  isCircleTurn = !isCircleTurn
-  setBoardHoverClass();
-};
-
-const handleClick = (e) => {
-  const cell = e.target;
-  const classToAdd = isCircleTurn ? 'circle' : 'x';
-
-  placeMark(cell, classToAdd);
-
-  // Verificar vitória
-  const isWin = checkForWin(classToAdd);
-  // Verificar por empate
-  const isDraw = checkForDraw();
-
-  if (isWin) {
-    endGame(false);
-  } else if (isDraw) {
-    endGame(true)
-  } else {
-    // Mudar símbolo
-    swapTurns();
-  }
-};
-startGame();
->>>>>>> dfe4170e130d90f45724a74021f54109d0a50ca4
 restartButton.addEventListener("click", startGame);
